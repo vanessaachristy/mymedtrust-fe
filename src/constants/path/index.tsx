abc9f2b5dd6c26ca349051f7abe904181f7919a5
Vanessa Christy Chandra <63032600+vanessaachristy@users.noreply.github.com>
2024-01-22 21:09:37 +0800

    [Path] Change path configurations

--- src/constants/path/index.tsx
+++ src/constants/path/index.tsx
@@ -1,3 +1,17 @@
+import { ReactNode } from "react";
+import { FaFileMedicalAlt } from "react-icons/fa";
+import {
+  FaFileMedical,
+  FaHouse,
+  FaUserCheck,
+  FaUserDoctor,
+  FaUserGroup,
+} from "react-icons/fa6";
+import { GiMedicinePills } from "react-icons/gi";
+import { IconType } from "react-icons/lib";
+import { MdNoFood } from "react-icons/md";
+import { PiFileMagnifyingGlassFill } from "react-icons/pi";
+
 export const enum PATH {
   Home = "/",
   Login = "/login",
@@ -34,84 +48,104 @@ export const enum PATH {
 export type NavItems = {
   label: string;
   href: PATH;
+  icon?: IconType;
 };
 
-export const PATIENT_NAV: NavItems[] = [
-  {
+const ALL_NAVS: Record<string, NavItems> = {
+  [PATH.Home]: {
     label: "Home",
     href: PATH.Home,
+    icon: FaHouse,
   },
-  {
+  [PATH.AllRecords]: {
     label: "All Records",
     href: PATH.AllRecords,
+    icon: FaFileMedical,
   },
-  {
+  [PATH.Observations]: {
     label: "Observations",
     href: PATH.Observations,
+    icon: PiFileMagnifyingGlassFill,
   },
-  {
+  [PATH.Conditions]: {
     label: "Conditions",
     href: PATH.Conditions,
+    icon: FaFileMedicalAlt,
   },
-  {
+  [PATH.Allergies]: {
     label: "Allergies",
     href: PATH.Allergies,
+    icon: MdNoFood,
   },
-  {
+  [PATH.Medications]: {
     label: "Medications",
     href: PATH.Medications,
+    icon: GiMedicinePills,
   },
-];
-
-export const DOCTOR_NAV: NavItems[] = [
-  {
-    label: "Home",
-    href: PATH.Home,
-  },
-  {
+  [PATH.AddObservation]: {
     label: "Add Observation",
     href: PATH.AddObservation,
+    icon: PiFileMagnifyingGlassFill,
   },
-  {
+  [PATH.AddCondition]: {
     label: "Add Condition",
     href: PATH.AddCondition,
+    icon: FaFileMedicalAlt,
   },
-  {
+  [PATH.AddAllergy]: {
     label: "Add Allergy",
     href: PATH.AddAllergy,
+    icon: MdNoFood,
   },
-  {
+  [PATH.AddMedication]: {
     label: "Add Medication",
     href: PATH.AddMedication,
+    icon: GiMedicinePills,
   },
-  {
+  [PATH.Patient]: {
     label: "Patients",
     href: PATH.Patient,
+    icon: FaUserGroup,
   },
-];
-
-export const ADMIN_NAV: NavItems[] = [
-  {
-    label: "Home",
-    href: PATH.Home,
-  },
-  {
-    label: "Patients",
-    href: PATH.Patient,
-  },
-  {
+  [PATH.Doctors]: {
     label: "Doctors",
     href: PATH.Doctors,
+    icon: FaUserDoctor,
   },
-  {
+  [PATH.Whitelist]: {
     label: "Whitelist",
     href: PATH.Whitelist,
+    icon: FaUserCheck,
   },
-];
-
-export const PROFILE_NAV: NavItems[] = [
-  {
+  [PATH.Profile]: {
     label: "Profile",
     href: PATH.Profile,
   },
+};
+
+export const PATIENT_NAV: NavItems[] = [
+  ALL_NAVS[PATH.Home],
+  ALL_NAVS[PATH.AllRecords],
+  ALL_NAVS[PATH.Observations],
+  ALL_NAVS[PATH.Conditions],
+  ALL_NAVS[PATH.Allergies],
+  ALL_NAVS[PATH.Medications],
 ];
+
+export const DOCTOR_NAV: NavItems[] = [
+  ALL_NAVS[PATH.Home],
+  ALL_NAVS[PATH.AddObservation],
+  ALL_NAVS[PATH.AddCondition],
+  ALL_NAVS[PATH.AddAllergy],
+  ALL_NAVS[PATH.AddMedication],
+  ALL_NAVS[PATH.Patient],
+];
+
+export const ADMIN_NAV: NavItems[] = [
+  ALL_NAVS[PATH.Home],
+  ALL_NAVS[PATH.Patient],
+  ALL_NAVS[PATH.Doctors],
+  ALL_NAVS[PATH.AllRecords],
+];
+
+export const PROFILE_NAV: NavItems[] = [ALL_NAVS[PATH.Profile]];
